4be8c666a4ddaca06fac1cc8273c82b731aa9c4d
Silicasandwhich <SilicaSandwhich@gmail.com>
2023-05-17 08:14:37 -0400

    alacritty and leftwm config

--- install.sh
+++ install.sh
@@ -20,5 +20,14 @@ echo "DONE"
 echo -n "copying onagre config..."
 cp -rf onagre ~/.config
 echo "DONE"
+echo -n "copying alacritty config..."
+cp -f ./alacritty.yml ~/.config/alacritty
+echo "DONE"
 
 echo "Done Installing!"
+read -p "Copy new config file (Y/n): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] || $confirm != [Nn] ]] || exit 1
+echo -n "copying leftwm config..."
+mv ~/.config/leftwm/config.ron ~/.config/leftwm/config-old.ron
+cp ./config.ron ~/.config/leftwm/config.ron
+echo "DONE"
+echo "Your old config file has been moved to ~/.config/leftwm/config-old.ron. Take a look at the new config and make any changes you want."
